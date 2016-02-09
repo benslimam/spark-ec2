2061389ff7677626a6bbb877f149bd60768cffef
dmt <dmt@no-mail.com>
2016-02-09 18:27:12 +0100

    Revert some changes about proxy and hdfs

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -9,15 +9,12 @@ if [ -n {{spark_worker_instances}} ]; then
 fi
 export SPARK_WORKER_CORES={{spark_worker_cores}}
 
-export HADOOP_HOME="/root/persistent-hdfs"
+export HADOOP_HOME="/root/ephemeral-hdfs"
 export SPARK_MASTER_IP={{active_master}}
 export MASTER=`cat /root/spark-ec2/cluster-url`
-export SPARK_PRIVATE_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/local-hostname`
-export HDFS_URL=hdfs://$SPARK_PRIVATE_DNS:9010
 
-export SPARK_SUBMIT_LIBRARY_PATH="$SPARK_SUBMIT_LIBRARY_PATH:/root/persistent-hdfs/lib/native/"
-export SPARK_SUBMIT_CLASSPATH="$SPARK_CLASSPATH:$SPARK_SUBMIT_CLASSPATH:/root/persistent-hdfs/conf"
-export SPARK_LOCAL_DIRS="/vol0/spark, /vol1/spark"
+export SPARK_SUBMIT_LIBRARY_PATH="$SPARK_SUBMIT_LIBRARY_PATH:/root/ephemeral-hdfs/lib/native/"
+export SPARK_SUBMIT_CLASSPATH="$SPARK_CLASSPATH:$SPARK_SUBMIT_CLASSPATH:/root/ephemeral-hdfs/conf"
 
 # Bind Spark's web UIs to this machine's public EC2 hostname otherwise fallback to private IP:
 SPARK_PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
@@ -27,7 +24,7 @@ fi
 export SPARK_PUBLIC_DNS
 
 # Used for YARN model
-export YARN_CONF_DIR="/root/persistent-hdfs/conf"
+export YARN_CONF_DIR="/root/ephemeral-hdfs/conf"
 
 # Set a high ulimit for large shuffles, only root can do this
 if [ $(id -u) == "0" ]
